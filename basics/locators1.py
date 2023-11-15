d9b352d01a6ca1ab0c8ff855060648057b3f235b
SDETEMRE <dincsoyemre@gmail.com>
2023-11-15 17:38:37 +0300

    locators

--- basics/locators1.py
+++ basics/locators1.py
@@ -3,11 +3,23 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as play:
     browser = play.chromium.launch(headless = False)
     page = browser.new_page()
-    page.goto('https://demo.automationtesting.in/')
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    #page.goto('https://demo.automationtesting.in/')
 
-    emailbox = page.wait_for_selector('#email')
-    emailbox.type('test@yahoo.com')
+    # emailbox = page.wait_for_selector('#email')
+    # emailbox.type('test@yahoo.com')
+    #
+    # submitbutton = page.wait_for_selector('#enterimg')
+    # submitbutton.click()
+    # page.wait_for_timeout(3000)
 
-    submitbutton = page.wait_for_selector('#enterimg')
-    submitbutton.click()
+    usernameBox = page.wait_for_selector('[name=username]')
+    usernameBox.type('Admin')
+
+    passwordBox = page.wait_for_selector('[type=password]')
+    passwordBox.type('admin123')
+
+    loginButton = page.wait_for_selector('[type=submit]')
+    loginButton.click()
+
     page.wait_for_timeout(3000)
